d3056f12cee0d9cb4c5bf1baeddc01c345f0fc58
Cyprien RUFFINO <ruffino.cyprien@protonmail.com>
2021-08-24 11:50:56 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,14 +7,14 @@ cp ~/.bashrc ~/.config_backups/
 cp bashrc ~/.bashrc
 
 mkdir ~/.local/bin
-cp -r bin ~./local/bin/
+cp -r bin ~/.local/bin/
 
 sudo cp -r icons /usr/share/
 sudo cp -r themes /usr/share/
 
 cp wallpaper.jpg ~/.wallpaper.jpg
 
-sudo pacman -Syy --noconfirm  wget gnome-shell firefox albert steam cups gnome-tweak-tool code
+sudo pacman -Syy --noconfirm  wget gnome-shell firefox steam cups gnome-tweak-tool code base-devel gnome-settings
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
 tar xvf yay.tar.gz
@@ -22,13 +22,12 @@ cd yay
 makepkg --noconfirm -si
 cd ..
 
-yay -Syy --noconfirm micro ferdi spotify jetbrains-toolbox evdi displaylink zotero freetube texstudio
+yay -Syy --noconfirm micro ferdi spotify jetbrains-toolbox zotero freetube texstudio albert
 
-gsettings set org.gnome.desktop.background draw-background false 
-gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.wallpaper.jpg 
+gsettings set org.gnome.desktop.background draw-background false
+gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.wallpaper.jpg
 gsettings set org.gnome.desktop.background draw-background true
 
 sudo systemctl enable cups
-sudo systemctl enable displaylink
 
 echo "Setup complete, reboot recommended and remember to sync extensions/tweaks"
