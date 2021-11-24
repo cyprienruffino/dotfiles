f92b80472c3bb450ee0c359ef4af73f41dc81c0a
Cyprien Ruffino <ruffino.cyprien@protonmail.com>
2021-11-24 13:01:13 +0100

    Adding git config

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ sudo cp -r themes /usr/share/
 
 cp wallpaper.jpg ~/.wallpaper.jpg
 
-sudo pacman -Syy --noconfirm zsh wget gnome-shell firefox cups gnome-tweak-tool code base-devel
+sudo pacman -Syy --noconfirm zsh wget gnome-shell firefox cups gnome-tweak-tool code base-devel wmctrl
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
 tar xvf yay.tar.gz
@@ -23,12 +23,16 @@ cd yay
 makepkg --noconfirm -si
 cd ..
 
-yay -Syy --noconfirm micro spotify jetbrains-toolbox zotero texstudio albert
+yay -Syy --noconfirm micro spotify jetbrains-toolbox lutris steam texstudio albert obsidian jabref
 
 gsettings set org.gnome.desktop.background draw-background false
 gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.wallpaper.jpg
 gsettings set org.gnome.desktop.background draw-background true
 
 sudo systemctl enable cups
+
+git config --global user.name "Cyprien Ruffino"
+git config --global user.mail "ruffino.cyprien@protonmail.com"
+git config --global credential.helper cache
 
 echo "Setup complete, reboot recommended and remember to sync extensions/tweaks"
