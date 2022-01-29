97797bd21c09e678886f2fee47fc3faafa53dc7a
Cyprien Ruffino <ruffino.cyprien@gmail.com>
2022-01-29 19:09:11 +0100

    Separating setup.sh and first_setup.sh

--- first_setup.sh
+++ first_setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 usermod --shell /usr/bin/zsh cyprien
 mkdir ~/.config_backups
 
@@ -11,6 +14,16 @@ sudo cp -r icons /usr/share/
 sudo cp -r themes /usr/share/
 
 cp wallpaper.jpg ~/.wallpaper.jpg
+
+sudo pacman -Syy --noconfirm zsh wget gnome-shell firefox cups gnome-tweak-tool code base-devel wmctrl
+
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
+tar xvf yay.tar.gz
+cd yay
+makepkg --noconfirm -si
+cd ..
+
+yay -Syy --noconfirm micro spotify jetbrains-toolbox lutris steam texstudio albert obsidian jabref
 
 gsettings set org.gnome.desktop.background draw-background false
 gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.wallpaper.jpg
