1ee835d1951472fbf42b79f08f6bab7d04f66c36
Cyprien Ruffino <ruffino.cyprien@protonmail.com>
2024-01-25 14:30:38 +0100

    cleanup

--- first_setup.sh
+++ first_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syy --noconfirm zsh wget firefox cups gnome-tweak-tool code base-devel wmctrl pacman-contrib bluez-utils dust bat exa
+sudo pacman -Syy --noconfirm $(cat packages)
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
 tar xvf yay.tar.gz
@@ -8,6 +8,6 @@ cd yay
 makepkg --noconfirm -si
 cd ..
 
-yay -Syy --noconfirm micro spotify jetbrains-toolbox lutris ulauncher obsidian ferdium freetube chrome-gnome-shell plank htop tilda gnome-shell-extension-openweather-git gnome-shell-extension-dash-to-plank gnome-shell-extension-pixel-saver-git gnome-shell-extension-hidetopbar-git
+yay -Syy --noconfirm $(cat aur_packages)
 
 sudo bash ./setup.sh
